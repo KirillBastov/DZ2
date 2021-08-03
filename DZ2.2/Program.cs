834cfb570e1b2b9fdde9d3acc4f43ce3612e5337
KirillBastov <shvetskirill1994@gmail.com>
2021-08-03 22:55:22 +0300

    no message

---
+++ DZ2.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите высоту треугольника: ");
+            int vys = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < vys; i++)
+            {
+                for (int j = vys; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int o = vys - 2 * i; o <= vys; o++)
+                {
+                    Console.Write("^");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
